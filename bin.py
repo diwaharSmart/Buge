28bd023529f4b8203d5e7eecb0b74fdb4cafa0a6
diwaharSmart <92229476+diwaharSmart@users.noreply.github.com>
2022-09-27 16:01:21 +0530

    update

--- bin.py
+++ bin.py
@@ -1,27 +1,30 @@
-# import argparse
-#
-# # Initializing Parser
-# parser = argparse.ArgumentParser(description='sort some integers.')
-#
-# # Adding Argument
-# parser.add_argument('integers',
-#                     metavar='N',
-#                     type=int,
-#                     nargs='+',
-#                     help='an integer for the accumulator')
-#
-# parser.add_argument(dest='accumulate',
-#                     action='store_const',
-#                     const=sorted,
-#                     help='arranges the integers in ascending order')
-#
-# args = parser.parse_args()
-# print(args.accumulate(args.integers))
-
+import argparse
 import os
-os.putenv("debug","true")
+import json
 
-cmd = "py buge/manage.py runserver 127.0.0.1:8060"
+def start_buge_server():
 
-returned_value = os.system(cmd)  # returns the exit code in unix
-print('returned value:', returned_value)
+    # Initializing Parser
+    all_args = argparse.ArgumentParser()
+
+    # Add arguments to the parser
+    all_args.add_argument("-c", "--conf", required=True,
+                          help="PATH OF THE CONFIGURATION FILE")
+
+    args = vars(all_args.parse_args())
+
+    with open(args["conf"], "r") as file:
+        config = json.load(file)
+
+    for key, value in config.items():
+        os.putenv(key, value)
+
+    cmd = "py buge/manage.py runserver "+str(config["ip_address"])+":"+str(config["port"])
+    os.system(cmd)
+    return
+
+
+
+if __name__ == "__main__":
+    start_buge_server()
+
--- buge/buge/settings.py
+++ buge/buge/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -78,8 +80,12 @@ else:
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'buge',
+        'USER': 'postgres',
+        'PASSWORD': 'Diwah@r123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
